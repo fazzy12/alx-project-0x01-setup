bfdb202216cff36d4eea14aec8b4dc37909cb053
ifeanyi kalu <kaluifeanyi00@gmail.com>
2025-09-20 13:45:08 +0100

    Implement Users Card Components

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -4,7 +4,7 @@ import { UserProps } from '@/interfaces/index';
 
 const UserCard: React.FC<UserProps> = ({ user }) => {
   return (
-    <div className="bg-white rounded-xl shadow-lg p-6 m-4 w-full md:w-1/3 transition-transform duration-300 transform hover:scale-105">
+    <div className="bg-blue rounded-xl shadow-lg p-6 m-4 w-full md:w-1/3 transition-transform duration-300 transform hover:scale-105">
       <h2 className="text-2xl font-bold text-gray-800">{user.name}</h2>
       <p className="text-md text-blue-500 font-semibold mt-1">@{user.username}</p>
       <div className="mt-4 text-sm text-gray-600">
